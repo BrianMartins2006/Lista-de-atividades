fcd2a6c929310ed4ef746d54dd986e1796e2846a
BrianMartins2006 <brianmartins997232779@gmail.com>
2024-09-26 07:42:20 -0300

    Add files via upload

---
+++ funcao.media.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static float Media(float num1, float num2, float num3)
+    {
+        float resultado;
+        resultado = (num1 + num2 + num3) / 3;
+        return resultado;
+    }
+    static void Main()
+    {
+        float n1, n2, n3, resultado;
+        Console.WriteLine("Digite três valores");
+        n1 = float.Parse(Console.ReadLine());
+        n2 = float.Parse(Console.ReadLine());
+        n3 = float.Parse(Console.ReadLine());
+
+        resultado = Media(n1, n2, n3);
+
+        Console.WriteLine("media: " + resultado);
+        Console.ReadKey();
+    }
+}
