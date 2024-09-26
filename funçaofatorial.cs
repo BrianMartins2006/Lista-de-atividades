40c39a6b59f2b23938d82a5a62bcb0fc0d01ff7c
BrianMartins2006 <brianmartins997232779@gmail.com>
2024-09-26 09:54:49 -0300

    Add files via upload

---
+++ funçaofatorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static int CalcularFatorial(int num)
+    {
+        int result = 1;
+
+        if (num > 0)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+        }
+        return result;
+    }
+    static void Main()
+    {
+        int n,fat;
+
+        Console.WriteLine("Digite o numero que deseja fatorial");
+        n = int.Parse(Console.ReadLine());
+
+        fat = CalcularFatorial(n);
+
+      
+            Console.WriteLine($"O fatoriade de {n} é: {fat}");
+            Console.ReadKey();
+    }
+}
