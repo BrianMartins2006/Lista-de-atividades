a9b3bf23f202a15decf3bc84b3d9cd010493f6f8
BrianMartins2006 <brianmartins997232779@gmail.com>
2024-09-26 09:02:48 -0300

    Add files via upload

---
+++ Funçaoprimo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static bool EhPrimo(int num)
+    {
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        int n;
+
+        Console.WriteLine("Digite um numero para verificar se é primo");
+        n = int.Parse(Console.ReadLine());
+
+        bool primo = EhPrimo(n);
+
+        if (primo == true)
+        {
+            Console.WriteLine($"O numero {n} é primo");
+        }
+        else
+        {
+            Console.WriteLine($"O numero {n} não é primo");
+        }
+        Console.ReadKey();
+    }
+}
