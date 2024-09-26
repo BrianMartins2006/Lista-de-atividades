b819b77d71dd440a7f2d39ccb02a9fd98e28f33c
BrianMartins2006 <brianmartins997232779@gmail.com>
2024-09-26 10:07:14 -0300

    Add files via upload

---
+++ funçaotemperatura.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static double ConverterParaFahrenheit(double temp) 
+    {
+        double result;
+        result = ((temp * 9 / 5) + 32);
+        return result;
+    }
+
+    static void Main()
+    {
+        double temperatura,fa;
+
+        Console.WriteLine("Digite a temperatura para ser convertida");
+        temperatura = double.Parse(Console.ReadLine());
+
+         fa = ConverterParaFahrenheit(temperatura);
+
+            Console.WriteLine($"{temperatura:F2} em Fahrenheit é: {fa} ");
+            Console.ReadKey();
+    }
+}
