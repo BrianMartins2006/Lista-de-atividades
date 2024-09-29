3145081ba115d2bb27fb9075d8172df8f402ae4a
BrianMartins2006 <brianmartins997232779@gmail.com>
2024-09-29 12:07:19 -0300

    Add files via upload

---
+++ Potencia.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+
+class Program
+{
+    static double CalcularPotencia(double baseN, double expoente)
+    {
+        double result;
+        result = (double)Math.Pow(baseN, expoente);
+        return result;
+    }
+    static void Main()
+    {
+        double basenum,expoente,pot;
+
+        Console.WriteLine("Informe a base: ");
+        basenum = Double.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o expoente: ");
+        expoente = Double.Parse(Console.ReadLine());
+
+        pot = CalcularPotencia(basenum,expoente);
+
+        Console.WriteLine($"A potencia de {basenum} elevado a {expoente} é: {pot}");
+        Console.ReadKey();
+    }
+}
