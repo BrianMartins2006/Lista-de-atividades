05c8012ad619ec730f3c5c63346737e77cb3281a
BrianMartins2006 <brianmartins997232779@gmail.com>
2024-10-11 07:57:21 -0300

    Add files via upload

---
+++ Funcaodestring.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o primeiro texto:");
+        string texto1 = Console.ReadLine();
+
+        Console.WriteLine("Digite o segundo texto:");
+        string texto2 = Console.ReadLine();
+
+        bool saoIguais = string.Compare(texto1.ToUpper(), texto2.ToUpper()) == 0;
+        Console.WriteLine("Os textos são iguais? " + saoIguais);
+
+        Console.WriteLine("Quantidade de caracteres no primeiro texto: " + texto1.Length);
+        Console.WriteLine("Quantidade de caracteres no segundo texto: " + texto2.Length);
+    }
+}
